7aa7bc494f27032722a82f01ec118cd23693844b
Rupal Saxena <rupal.saxena@uzh.ch>
2022-03-10 02:53:33 +0100

    code cleaning

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import AppRouter from "components/routing/routers/AppRouter";
 /**
  * Happy coding!
  * React Template by Lucas Pelloni
- * Overhauled by Kyrill Hux
+ * Overhauled by Kyrill Hux and Rupal Saxena
  */
 const App = () => {
   return (
     <div>
       <Header height="100"/>
-
       <AppRouter/>
     </div>
   );
